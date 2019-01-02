ea596461166ec87c38f55d993acf410004bd753c
NassrEML <>
2019-01-02 20:03:42 +0100

    Segunda version de Kata4

--- src/Controlador/Kata4.java
+++ src/Controlador/Kata4.java
@@ -13,22 +13,38 @@ import java.util.List;
  */
 
 public class Kata4 {
-
+    
+    //private static HistogramDisplay histoDisplay;
+    private static MailListReader lecturaLista;
+    private static String fileName;
+    private static List<Mail> mailList;
+    private static MailHistogramBuilder histogramBuild;
+    private static Histogram<String> histogram;
+    
     public static void main(String[] args) throws IOException {
-        
-        MailListReader lecturaLista = new MailListReader();
-        
-        String fileName = "email.txt";
-        
-        List<Mail> mailList = lecturaLista.read(fileName);
-        
-        MailHistogramBuilder histogramBuild = new MailHistogramBuilder();
-        
-        Histogram<String> histogram = histogramBuild.build(mailList);
-        
+        execute();
+    }
+    
+    private static void execute(){
+        input();
+        process();
+        output();
+    }
+
+    private static void input(){
+        lecturaLista = new MailListReader();
+        fileName = "email.txt";
+        mailList = lecturaLista.read(fileName);
+    }
+
+    private static void process(){
+        histogramBuild = new MailHistogramBuilder();
+        histogram = histogramBuild.build(mailList);
+    }
+
+    private static void output(){
         HistogramDisplay histo = new HistogramDisplay(histogram);
         
         histo.execute();
-        
     }
 }
